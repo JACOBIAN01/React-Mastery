b3c11ccd442f14549def01b9265a068f01f52a13
JACOBIAN01 <subhadeepghorai23@gmail.com>
2025-03-01 14:25:08 +0530

    UPDATE

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,16 +1,15 @@
 import './App.css'
-import element from './NewApp';
-import Fruits from './List';
-import People from './User';
+// import element from './NewApp';
+// import Fruits from './List';
+// import People from './User';
+
 
 function App() {
 
 
   return (
      <>
-    {element}
-    <Fruits/>
-    <People/>
+   
     </>
   )
 }
