c101aff8edf85ffbd75715c3409a7bb237ff376f
JACOBIAN01 <subhadeepghorai23@gmail.com>
2026-06-15 00:10:45 +0530

    feat: add @tanstack/react-query for data fetching and create Fruits component for wallet management

---
+++ react_arena/src/components/Fruits.jsx
@@ -0,0 +1,20 @@
+import { useReducer } from "react";
+
+const reducer = (state, action) => {
+  if (action.type == "buy") return { money: state.money - 10 };
+  if (action.type == "sell") return { money: state.money + 10 };
+  return state;
+};
+
+export default function Fruits() {
+  const initialState = { money: 10 };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  console.log(state);
+  return (
+    <div className="m-10">
+      <p>Wallet: {state.money}</p>
+      <button className="bg-amber-600 px-2 py-0.5 text-white m-1 rounded-sm" onClick={() => dispatch({ type: "buy" })}>Buy</button>
+      <button className="bg-amber-600 px-2 py-0.5 text-white m-1 rounded-sm" onClick={() => dispatch({ type: "sell" })}>Sell</button>
+    </div>
+  );
+}
