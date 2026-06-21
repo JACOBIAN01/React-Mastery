257e0aa8b37986709e0fa3b42cd5a8806ddecc91
JACOBIAN01 <subhadeepghorai23@gmail.com>
2026-06-21 23:47:52 +0530

    feat: implement joke fetching functionality using @tanstack/react-query in App component

--- react_arena/src/App.jsx
+++ react_arena/src/App.jsx
@@ -1,9 +1,23 @@
-
+import { useQuery } from "@tanstack/react-query";
 
 export default function App() {
+  const {data} = useQuery({
+    queryKey: ["jokes"],
+    queryFn: getJoke,
+  });
 
-  return (
-    <>
-    </>
-  );
+  return (<div>
+    {JSON.stringify(data.setup)}
+    </div>);
+}
+
+async function getJoke() {
+  try {
+    const res = await fetch(
+      "https://official-joke-api.appspot.com/random_joke",
+    ).then((res) => res.json());
+    return res;
+  } catch (err) {
+    console.log(err);
+  }
 }
